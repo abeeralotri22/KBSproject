cb077d2bdf75f3f86a97abfa3600b0a16e1caa81
Batoul Allahham <batoolallahham5@gmail.com>
2026-08-09 18:30:56 +0300

    added the role

--- api_app/models.py
+++ api_app/models.py
@@ -2,10 +2,16 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
+    ROLE_CHOICES = (
+        ('customer', 'Customer'),
+        ('admin', 'Admin'),
+    )
+
     first_name =models.CharField(max_length=255, blank=True, null=True)
     last_name = models.CharField(max_length=255, blank=True, null=True)
     email = models.EmailField(unique=True)
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='customer')
     profile_image = models.ImageField(upload_to='users/', blank=True, null=True)
 
     def __str__(self):
-        return self.email
+        return f"{self.email} ({self.role})"
