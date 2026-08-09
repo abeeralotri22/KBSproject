71e245c5a78e18f6ad0d7f855df29d06fb91fb0b
Batoul Allahham <batoolallahham5@gmail.com>
2026-08-09 17:16:50 +0300

    added the simple jwt APIs

--- api_app/urls.py
+++ api_app/urls.py
@@ -1,8 +1,19 @@
 from django.urls import path
 from . import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
-
+    #register & login
     path('register/', views.register, name='register'),
-    path('login/', views.login, name = 'login')
+    path('login/', views.login, name = 'login'),
+    #built-in
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    #Profile
+    path('profile/', views.profile, name = 'profile')
+
 ]
