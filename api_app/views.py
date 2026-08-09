fa22539505ae0229206304f9d4c3762c8a41f4f9
Batoul Allahham <batoolallahham5@gmail.com>
2026-08-09 16:36:17 +0300

    added the login view

--- api_app/views.py
+++ api_app/views.py
@@ -31,6 +31,25 @@ def register(request):
     return Response({"errors": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
 
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def login(request):
+    email = request.data.get('email')
+    password = request.data.get('password')
+    user = authenticate(username=email, password=password)
 
+    if user:
+        refresh = RefreshToken.for_user(user)
+        return Response({
+            "message": f"logged in successfully",
+            "user": {
+                "id": user.id,
+                "email": user.email,
+            },
+            "access_token": str(refresh.access_token),
+            "refresh_token": str(refresh),
+        }, status=status.HTTP_200_OK)
+
+    return Response({"error": "Invalid email or password"}, status=status.HTTP_401_UNAUTHORIZED)
 
 
