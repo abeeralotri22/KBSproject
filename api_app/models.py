1db8b488b50846fcbfcf379f1a9930635a68aa23
Batoul Allahham <batoolallahham5@gmail.com>
2026-08-09 16:34:33 +0300

    added the user model

---
+++ api_app/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class CustomUser(AbstractUser):
+    first_name =models.CharField(max_length=255, blank=True, null=True)
+    last_name = models.CharField(max_length=255, blank=True, null=True)
+    email = models.EmailField(unique=True)
+    profile_image = models.ImageField(upload_to='users/', blank=True, null=True)
+
+    def __str__(self):
+        return self.email
