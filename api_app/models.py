6ea6fb103a7c9e4dfbc983b974a2068ae15a025a
Batoul Allahham <batoolallahham5@gmail.com>
2026-08-12 23:27:58 +0300

    Added the subject and the lesson models

--- api_app/models.py
+++ api_app/models.py
@@ -1,17 +1,42 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
 class CustomUser(AbstractUser):
     ROLE_CHOICES = (
         ('customer', 'Customer'),
         ('admin', 'Admin'),
     )
 
-    first_name =models.CharField(max_length=255, blank=True, null=True)
+    first_name = models.CharField(max_length=255, blank=True, null=True)
     last_name = models.CharField(max_length=255, blank=True, null=True)
     email = models.EmailField(unique=True)
     role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='customer')
     profile_image = models.ImageField(upload_to='users/', blank=True, null=True)
 
+    subjects = models.ManyToManyField('Subject', blank=True, related_name='enrolled_users')
+
     def __str__(self):
         return f"{self.email} ({self.role})"
+
+
+class Subject(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Lesson(models.Model):
+    content = models.TextField()
+    order = models.PositiveIntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE, related_name='lessons')
+
+
+    class Meta:
+        ordering = ['order', 'created_at']
+
+    def __str__(self):
+        return f"{self.subject.name}"
