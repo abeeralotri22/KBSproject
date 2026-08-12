a137cc8093a3195bfab3f279d335ae4a09cacca2
Batoul Allahham <batoolallahham5@gmail.com>
2026-08-12 23:28:14 +0300

    Added the subject serializer

--- api_app/serializers.py
+++ api_app/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import CustomUser
+from .models import CustomUser, Subject
 
 
 class RegisterSerializer(serializers.ModelSerializer):
@@ -72,3 +72,10 @@ class ChangePasswordSerializer(serializers.Serializer):
         user.set_password(self.validated_data['new_password'])
         user.save()
         return user
+
+
+######### Subjects
+class SubjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Subject
+        fields = ['id', 'name']
